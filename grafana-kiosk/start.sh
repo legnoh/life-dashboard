272e62512c0385e2ffaf75b402c57f6b867fc4ad
Ryoma Kai <legknmj@icloud.com>
2021-09-26 00:09:55 +0900

    bugfix

--- grafana-kiosk/start.sh
+++ grafana-kiosk/start.sh
@@ -8,5 +8,8 @@ envsubst < ./tpl/grafana-kiosk.yaml.tpl > /tmp/grafana-kiosk.yaml
 sudo cp -r /tmp/grafana-kiosk.yaml /etc/grafana-kiosk.yaml
 sudo chmod 664 /etc/grafana-kiosk.yaml
 
+mkdir -p /home/pi/.config/lxsession/LXDE-pi/
+touch /home/pi/.config/lxsession/LXDE-pi/autostart
 echo "/usr/bin/grafana-kiosk -c /etc/grafana-kiosk.yaml" > /home/pi/.config/lxsession/LXDE-pi/autostart
+
 cd -
