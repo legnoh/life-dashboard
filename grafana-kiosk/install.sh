c9ff0d201b143f9275e1f824ce5c32dc6e2f1e0e
Ryoma Kai <legknmj@icloud.com>
2021-09-25 23:44:07 +0900

    bugfix

--- grafana-kiosk/install.sh
+++ grafana-kiosk/install.sh
@@ -10,7 +10,8 @@ ARCH="armv7"
 # install some utilities
 sudo apt-get -y install \
     chromium-browser \
-    jq
+    jq \
+    unclutter
 
 # install latest grafana-kiosk
 latest_info=$(curl -s https://api.github.com/repos/grafana/grafana-kiosk/releases/latest)
@@ -18,3 +19,7 @@ download_url=$(echo ${latest_info} | jq -r ".assets[] | select(.name | contains(
 curl -OL ${download_url}
 sudo cp -p grafana-kiosk.linux.${ARCH} /usr/bin/grafana-kiosk
 sudo chmod 755 /usr/bin/grafana-kiosk
+
+# change the display resolution to 1920x1080
+# https://www.raspberrypi.org/documentation/computers/config_txt.html#hdmi_mode
+sudo sed -i 's/#hdmi_mode=1/hdmi_mode=82/' /boot/config.txt
