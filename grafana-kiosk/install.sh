70bae00551204cc634525f18f18585c6293680d0
Ryoma Kai <legknmj@icloud.com>
2021-09-25 21:37:49 +0900

    bugfix

--- grafana-kiosk/install.sh
+++ grafana-kiosk/install.sh
@@ -13,6 +13,6 @@ sudo apt-get -y install \
 # install latest grafana-kiosk
 latest_info=$(curl -s https://api.github.com/repos/grafana/grafana-kiosk/releases/latest)
 download_url=$(echo ${latest_info} | jq -r '.assets[] | select(.name | contains("${ARCH}")) | .browser_download_url')
-curl -OL ${url}
+curl -OL ${download_url}
 sudo cp -p grafana-kiosk.linux.${ARCH} /usr/bin/grafana-kiosk
 sudo chmod 755 /usr/bin/grafana-kiosk
