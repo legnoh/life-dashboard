602a06432c17ea91756f2b613eeae7ed73caf47d
Ryoma Kai <legknmj@icloud.com>
2021-09-25 22:45:47 +0900

    bugfix

--- grafana-kiosk/start.sh
+++ grafana-kiosk/start.sh
@@ -5,8 +5,10 @@ set -eu
 cd grafana-kiosk
 
 sudo cp -r ./tpl/grafana-kiosk.service /etc/systemd/system/grafana-kiosk.service
-sudo sh -c 'envsubst < ./tpl/grafana-kiosk.yaml.tpl > /etc/grafana-kiosk.yaml'
 sudo chmod 664 /etc/systemd/system/grafana-kiosk.service
+
+envsubst < ./tpl/grafana-kiosk.yaml.tpl > /tmp/grafana-kiosk.yaml
+sudo cp -r /tmp/grafana-kiosk.yaml /etc/grafana-kiosk.yaml
 sudo chmod 664 /etc/grafana-kiosk.yaml
 
 sudo systemctl daemon-reload
