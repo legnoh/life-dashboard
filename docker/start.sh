391da8e3411f934f9205ab9fdd58c085e2a6b53a
Ryoma Kai <legknmj@icloud.com>
2021-09-26 23:03:15 +0900

    bugfix

--- docker/start.sh
+++ docker/start.sh
@@ -9,8 +9,10 @@ envsubst < ./tpl/tado.env.tpl > ./tado.env
 envsubst < ./tpl/todoist.env.tpl > ./todoist.env
 envsubst < ./tpl/prometheus.yaml.tpl > ./prometheus/prometheus.yaml
 
-sudo docker-compose stop --remove-orphans
+sudo docker-compose stop
 sudo docker-compose up -d
 sudo docker ps
+
+sudo docker system prune -f --all --volumes
 
 cd -
