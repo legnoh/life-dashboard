caf4099a6b05bef8e01bab02befa8a100236b57f
Ryoma Kai <legknmj@icloud.com>
2021-09-26 13:45:37 +0900

    bugfix

--- docker/start.sh
+++ docker/start.sh
@@ -9,7 +9,7 @@ envsubst < ./tpl/tado.env.tpl > ./tado.env
 envsubst < ./tpl/todoist.env.tpl > ./todoist.env
 envsubst < ./tpl/prometheus.yaml.tpl > ./prometheus/prometheus.yaml
 
-sudo docker-compose down --remove-orphans
+sudo docker-compose stop --remove-orphans
 sudo docker-compose up -d --force-recreate
 sudo docker ps
 
