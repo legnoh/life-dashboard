33f408b14df3abf2c29dc4bd48f676eaacc14d87
Ryoma Kai <legknmj@icloud.com>
2022-04-18 22:20:14 +0900

    convert withings-exporter to daemon process exporter

--- docker/start.sh
+++ docker/start.sh
@@ -8,6 +8,7 @@ envsubst < ./tpl/openweather.env.tpl > ./openweather.env
 envsubst < ./tpl/tado.env.tpl > ./tado.env
 envsubst < ./tpl/todoist.env.tpl > ./todoist.env
 envsubst < ./tpl/smartmeter.env.tpl > ./smartmeter.env
+envsubst < ./tpl/withings.env.tpl > ./withings.env
 envsubst < ./tpl/prometheus.yaml.tpl > ./prometheus/prometheus.yaml
 
 sudo docker-compose pull
