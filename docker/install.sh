3e031d258750a0d7b48ee5a4fa6321256601f6d0
Ryoma Kai <legknmj@icloud.com>
2022-07-03 12:13:48 +0900

    change docker-compose to docker compose v2

--- docker/install.sh
+++ docker/install.sh
@@ -13,18 +13,15 @@ sudo apt-get -y install \
     gnupg \
     lsb-release
 
-## install docker with official script
-## https://docs.docker.com/engine/install/debian/#install-using-the-convenience-script
-which docker > /dev/null
-if [ $? != 0 ]
-then
-    sudo curl -fsSL https://get.docker.com -o get-docker.sh
-    sudo sh get-docker.sh
-fi
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg \
+    | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" \
+  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# install docker-compose
-which docker-compose > /dev/null
-if [ $? != 0 ]
-then
-    sudo pip3 install docker-compose
-fi
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
--- docker/start.sh
+++ docker/start.sh
@@ -13,9 +13,9 @@ envsubst < ./tpl/smartmeter.env.tpl > ./smartmeter.env
 envsubst < ./tpl/withings.env.tpl > ./withings.env
 envsubst < ./tpl/prometheus.yaml.tpl > ./prometheus/prometheus.yaml
 
-sudo docker-compose pull
-sudo docker-compose stop
-sudo docker-compose up -d
+sudo docker compose pull
+sudo docker compose stop
+sudo docker compose up -d
 sudo docker ps
 
 sudo docker system prune -f --all --volumes
