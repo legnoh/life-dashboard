4c70b6e3d569972684684105cd162cc01704c580
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2023-04-30 17:35:36 +0900

    bugfix

--- grafstation/scripts/start-docker-compose.sh
+++ grafstation/scripts/start-docker-compose.sh
@@ -5,7 +5,7 @@ set -e
 COMPOSE_FILE="${HOME}/life-dashboard/configs/docker-compose.yml"
 
 docker compose -f ${COMPOSE_FILE} pull
-docker compose -f ${COMPOSE_FILE} down -f
+docker compose -f ${COMPOSE_FILE} down
 docker compose -f ${COMPOSE_FILE} up -d
 docker ps
 
