7594800785d04e14d5a5b0daaa290946d766ef58
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2023-05-09 20:45:21 +0900

    bugfix of playlist

--- grafstation/configs/grafana/grafana_playlist.tf
+++ grafstation/configs/grafana/grafana_playlist.tf
@@ -2,7 +2,9 @@ resource "grafana_playlist" "life-dashboard-refresh" {
   name = "life-dashboard-refresh"
   interval = "15m"
   item {
-    title = grafana_dashboard.life-metrics.uid
+    title = "Life Metrics"
+    type = "dashboard_by_uid"
+    value = grafana_dashboard.life-metrics.uid
     order = 1
   }
 }
