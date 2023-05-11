9a3c5333fdbb2463048b6afc478254910ad14c38
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2023-05-11 23:28:09 +0900

    add video html to grafana

--- grafstation/configs/grafana/grafana_panel-tv.tf
+++ grafstation/configs/grafana/grafana_panel-tv.tf
@@ -1,100 +1,51 @@
 resource "grafana_library_panel" "tv" {
   name = "TV(1ch)"
   model_json = jsonencode({
-    type        = "aidanmountford-html-panel",
+    type        = "innius-video-panel",
     transparent = true,
-    html_data   = <<-EOT
-    <video autoplay controls width="100%" height="100%">
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/m2ts?mode=0"
-        type="application/x-mpegURL"
-      >
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/webm?mode=0"
-        type="video/webm"
-      >
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/mp4?mode=0"
-        type="video/mp4"
-      >
-      Your browser does not support the video tag.
-    </video>
-    EOT
+    options = {
+      autoPlay  = true,
+      videoType = "iframe",
+      iframeURL = "http://${var.TV_EPGSTATION_HOST}/player/mpegts.html?channel=${var.TV_CHANNEL_ID1}",
+    },
   })
 }
 
 resource "grafana_library_panel" "tv-muted" {
   name = "TV(1ch/mute)"
   model_json = jsonencode({
-    type        = "aidanmountford-html-panel",
+    type        = "innius-video-panel",
     transparent = true,
-    html_data   = <<-EOT
-    <video autoplay controls muted width="100%" height="100%">
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/m2ts?mode=0"
-        type="application/x-mpegURL"
-      >
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/webm?mode=0"
-        type="video/webm"
-      >
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/mp4?mode=0"
-        type="video/mp4"
-      >
-      Your browser does not support the video tag.
-    </video>
-    EOT
+    options = {
+      autoPlay  = true,
+      videoType = "iframe",
+      iframeURL = "http://${var.TV_EPGSTATION_HOST}/player/mpegts.html?channel=${var.TV_CHANNEL_ID1}&muted=true",
+    },
   })
 }
 
 resource "grafana_library_panel" "tv2" {
   name = "TV(2ch)"
   model_json = jsonencode({
-    type        = "aidanmountford-html-panel",
+    type        = "innius-video-panel",
     transparent = true,
-    html_data   = <<-EOT
-    <video autoplay controls width="100%" height="100%">
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/m2ts?mode=0"
-        type="application/x-mpegURL"
-      >
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/webm?mode=0"
-        type="video/webm"
-      >
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/mp4?mode=0"
-        type="video/mp4"
-      >
-      Your browser does not support the video tag.
-    </video>
-    EOT
+    options = {
+      autoPlay  = true,
+      videoType = "iframe",
+      iframeURL = "http://${var.TV_EPGSTATION_HOST}/player/mpegts.html?channel=${var.TV_CHANNEL_ID2}",
+    },
   })
 }
 
 resource "grafana_library_panel" "tv2-muted" {
   name = "TV(2ch/mute)"
   model_json = jsonencode({
-    type        = "aidanmountford-html-panel",
+    type        = "innius-video-panel",
     transparent = true,
-    html_data   = <<-EOT
-    <video autoplay controls muted width="100%" height="100%">
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/m2ts?mode=0"
-        type="application/x-mpegURL"
-      >
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/webm?mode=0"
-        type="video/webm"
-      >
-      <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/mp4?mode=0"
-        type="video/mp4"
-      >
-      Your browser does not support the video tag.
-    </video>
-    EOT
+    options = {
+      autoPlay  = true,
+      videoType = "iframe",
+      iframeURL = "http://${var.TV_EPGSTATION_HOST}/player/mpegts.html?channel=${var.TV_CHANNEL_ID2}&muted=true",
+    },
   })
 }
-
