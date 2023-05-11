214dae7683c2720805b6460e153f7ac2a4744df4
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2023-05-11 09:54:04 +0900

    revert

--- grafstation/configs/grafana/grafana_panel-tv.tf
+++ grafstation/configs/grafana/grafana_panel-tv.tf
@@ -6,12 +6,16 @@ resource "grafana_library_panel" "tv" {
     html_data   = <<-EOT
     <video autoplay controls width="100%" height="100%">
       <source
+        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/m2ts?mode=0"
+        type="application/x-mpegURL"
+      >
+      <source
         src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/webm?mode=0"
         type="video/webm"
       >
       <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/m2ts?mode=0"
-        type="application/x-mpegURL"
+        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/mp4?mode=0"
+        type="video/mp4"
       >
       Your browser does not support the video tag.
     </video>
@@ -27,12 +31,16 @@ resource "grafana_library_panel" "tv-muted" {
     html_data   = <<-EOT
     <video autoplay controls muted width="100%" height="100%">
       <source
+        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/m2ts?mode=0"
+        type="application/x-mpegURL"
+      >
+      <source
         src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/webm?mode=0"
         type="video/webm"
       >
       <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/m2ts?mode=0"
-        type="application/x-mpegURL"
+        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID1}/mp4?mode=0"
+        type="video/mp4"
       >
       Your browser does not support the video tag.
     </video>
@@ -48,12 +56,16 @@ resource "grafana_library_panel" "tv2" {
     html_data   = <<-EOT
     <video autoplay controls width="100%" height="100%">
       <source
+        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/m2ts?mode=0"
+        type="application/x-mpegURL"
+      >
+      <source
         src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/webm?mode=0"
         type="video/webm"
       >
       <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/m2ts?mode=0"
-        type="application/x-mpegURL"
+        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/mp4?mode=0"
+        type="video/mp4"
       >
       Your browser does not support the video tag.
     </video>
@@ -69,12 +81,16 @@ resource "grafana_library_panel" "tv2-muted" {
     html_data   = <<-EOT
     <video autoplay controls muted width="100%" height="100%">
       <source
+        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/m2ts?mode=0"
+        type="application/x-mpegURL"
+      >
+      <source
         src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/webm?mode=0"
         type="video/webm"
       >
       <source
-        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/m2ts?mode=0"
-        type="application/x-mpegURL"
+        src="http://${var.TV_EPGSTATION_HOST}/api/streams/live/${var.TV_CHANNEL_ID2}/mp4?mode=0"
+        type="video/mp4"
       >
       Your browser does not support the video tag.
     </video>
