3503c912f390fbef07d6a8b354a49626d99616ea
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2023-11-04 10:57:06 +0900

    add konomitv panel

--- grafstation/configs/grafana/grafana_panel-konomitv.tf
+++ grafstation/configs/grafana/grafana_panel-konomitv.tf
@@ -1,55 +1,55 @@
-resource "grafana_library_panel" "tv" {
+resource "grafana_library_panel" "konomitv" {
   org_id = grafana_organization.main.org_id
-  name = "TV(1ch)"
+  name = "KonomiTV(1ch)"
   model_json = jsonencode({
     type        = "innius-video-panel",
     transparent = true,
     options = {
       autoPlay  = true,
       videoType = "iframe",
-      iframeURL = "http://grafstation.local/player/epgstation.html?channel=${var.TV_CHANNEL_ID1}",
+      iframeURL = "http://grafstation.local/player/konomitv.html?channel=${var.TV_CHANNEL_ID1}",
     },
   })
 }
 
-resource "grafana_library_panel" "tv-muted" {
+resource "grafana_library_panel" "konomitv-muted" {
   org_id = grafana_organization.main.org_id
-  name = "TV(1ch/mute)"
+  name = "KonomiTV(1ch/mute)"
   model_json = jsonencode({
     type        = "innius-video-panel",
     transparent = true,
     options = {
       autoPlay  = true,
       videoType = "iframe",
-      iframeURL = "http://grafstation.local/player/epgstation.html?channel=${var.TV_CHANNEL_ID1}&muted=true",
+      iframeURL = "http://grafstation.local/player/konomitv.html?channel=${var.TV_CHANNEL_ID1}&muted=true",
     },
   })
 }
 
-resource "grafana_library_panel" "tv2" {
+resource "grafana_library_panel" "konomitv2" {
   org_id = grafana_organization.main.org_id
-  name = "TV(2ch)"
+  name = "KonomiTV(2ch)"
   model_json = jsonencode({
     type        = "innius-video-panel",
     transparent = true,
     options = {
       autoPlay  = true,
       videoType = "iframe",
-      iframeURL = "http://grafstation.local/player/epgstation.html?channel=${var.TV_CHANNEL_ID2}",
+      iframeURL = "http://grafstation.local/player/konomitv.html?channel=${var.TV_CHANNEL_ID2}",
     },
   })
 }
 
-resource "grafana_library_panel" "tv2-muted" {
+resource "grafana_library_panel" "konomitv2-muted" {
   org_id = grafana_organization.main.org_id
-  name = "TV(2ch/mute)"
+  name = "KonomiTV(2ch/mute)"
   model_json = jsonencode({
     type        = "innius-video-panel",
     transparent = true,
     options = {
       autoPlay  = true,
       videoType = "iframe",
-      iframeURL = "http://grafstation.local/player/epgstation.html?channel=${var.TV_CHANNEL_ID2}&muted=true",
+      iframeURL = "http://grafstation.local/player/konomitv.html?channel=${var.TV_CHANNEL_ID2}&muted=true",
     },
   })
 }
