11105aacc98c6c291635ee34ba6de8e66d23d293
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2024-03-04 23:04:56 +0900

    add general

--- grafstation/configs/grafana/grafana_panel-greench.tf
+++ grafstation/configs/grafana/grafana_panel-greench.tf
@@ -1,3 +1,7 @@
+locals {
+  gch_stream_url_encode = urlencode( var.GCH_STREAM_URL )
+}
+
 resource "grafana_library_panel" "greench" {
   org_id = grafana_organization.main.org_id
   name = "GREEN ch."
@@ -7,7 +11,21 @@ resource "grafana_library_panel" "greench" {
     options = {
       autoPlay  = true,
       videoType = "iframe",
-      iframeURL = "${var.GCH_STREAM_URL}",
+      iframeURL = "http://grafstation.local/player/general.html?url=" + local.gch_stream_url_encode,
+    },
+  })
+}
+
+resource "grafana_library_panel" "greench_muted" {
+  org_id = grafana_organization.main.org_id
+  name = "GREEN ch.(muted)"
+  model_json = jsonencode({
+    type        = "innius-video-panel",
+    transparent = true,
+    options = {
+      autoPlay  = true,
+      videoType = "iframe",
+      iframeURL = "http://grafstation.local/player/general.html?url=" + local.gch_stream_url_encode + "&muted=true",
     },
   })
 }
