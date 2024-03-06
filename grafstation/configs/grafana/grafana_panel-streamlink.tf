8d826e390d529b8b1e0f94e779e2e51b5b9ca5d9
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2024-03-06 23:17:21 +0900

    divide channel

--- grafstation/configs/grafana/grafana_panel-streamlink.tf
+++ grafstation/configs/grafana/grafana_panel-streamlink.tf
@@ -1,27 +1,55 @@
-resource "grafana_library_panel" "streamlink" {
+resource "grafana_library_panel" "stream1" {
   org_id = grafana_organization.main.org_id
-  name = "Streamlink"
+  name = "Streamlink(1ch)"
   model_json = jsonencode({
     type        = "innius-video-panel",
     transparent = true,
     options = {
       autoPlay  = true,
       videoType = "iframe",
-      iframeURL = "http://grafstation.local/player/mpegts.html?url=http%3A%2F%2Fgrafstation.local%2Fstreamlink",
+      iframeURL = "http://grafstation.local/player/mpegts.html?url=http%3A%2F%2Fgrafstation.local%2Fstream%2F1",
     },
   })
 }
 
-resource "grafana_library_panel" "streamlink-muted" {
+resource "grafana_library_panel" "stream1-muted" {
   org_id = grafana_organization.main.org_id
-  name = "Streamlink(muted)"
+  name = "Streamlink(1ch/muted)"
   model_json = jsonencode({
     type        = "innius-video-panel",
     transparent = true,
     options = {
       autoPlay  = true,
       videoType = "iframe",
-      iframeURL = "http://grafstation.local/player/mpegts.html?url=http%3A%2F%2Fgrafstation.local%2Fstreamlink&muted=true",
+      iframeURL = "http://grafstation.local/player/mpegts.html?url=http%3A%2F%2Fgrafstation.local%2Fstream%2F1&muted=true",
+    },
+  })
+}
+
+resource "grafana_library_panel" "stream2" {
+  org_id = grafana_organization.main.org_id
+  name = "Streamlink(2ch)"
+  model_json = jsonencode({
+    type        = "innius-video-panel",
+    transparent = true,
+    options = {
+      autoPlay  = true,
+      videoType = "iframe",
+      iframeURL = "http://grafstation.local/player/mpegts.html?url=http%3A%2F%2Fgrafstation.local%2Fstream%2F2",
+    },
+  })
+}
+
+resource "grafana_library_panel" "stream2-muted" {
+  org_id = grafana_organization.main.org_id
+  name = "Streamlink(2ch/muted)"
+  model_json = jsonencode({
+    type        = "innius-video-panel",
+    transparent = true,
+    options = {
+      autoPlay  = true,
+      videoType = "iframe",
+      iframeURL = "http://grafstation.local/player/mpegts.html?url=http%3A%2F%2Fgrafstation.local%2Fstream%2F2&muted=true",
     },
   })
 }
