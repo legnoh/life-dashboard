b5c0d7f98c378fbe91bed1642b5e27cdec782b47
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2024-03-18 00:37:53 +0900

    add sleep-bgm

--- grafstation/configs/grafana/variables.tf
+++ grafstation/configs/grafana/variables.tf
@@ -7,7 +7,7 @@ variable "TV_CHANNEL1" {
   type = string
   default = "nightmode-bgm"
   validation {
-    condition     = contains(["daymode-bgm", "nightmode-bgm", "stretch", "news-domestic", "news-global", "vtuber", "mahjong", "greench", "earthquake"], var.TV_CHANNEL1)
+    condition     = contains(["daymode-bgm", "nightmode-bgm", "sleep-bgm", "stretch", "news-domestic", "news-global", "vtuber", "mahjong", "greench", "earthquake"], var.TV_CHANNEL1)
     error_message = "This value is not permitted in condition!"
   }
 }
@@ -16,7 +16,7 @@ variable "TV_CHANNEL2" {
   type = string
   default = "vtuber"
   validation {
-    condition     = contains(["daymode-bgm", "nightmode-bgm", "stretch", "news-domestic", "news-global", "vtuber", "mahjong", "greench", "earthquake"], var.TV_CHANNEL2)
+    condition     = contains(["daymode-bgm", "nightmode-bgm", "sleep-bgm", "stretch", "news-domestic", "news-global", "vtuber", "mahjong", "greench", "earthquake"], var.TV_CHANNEL2)
     error_message = "This value is not permitted in condition!"
   }
 }
