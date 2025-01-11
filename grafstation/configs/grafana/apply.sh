abe2d37eb839dcd78d5383fadbc97714a2f4e55f
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2025-01-11 09:57:12 +0900

    bugfix

--- grafstation/configs/grafana/apply.sh
+++ grafstation/configs/grafana/apply.sh
@@ -87,7 +87,7 @@ function fetch_dirt_race_data() {
 # グリーンチャンネルの指定チャンネルでストリームURLを取得する
 function get_greench_stream_url() {
   local channel_code=${1:-"1"}
-  ${STREAMLINK} "https://sp.gch.jp/#ch${channel_codee}" \
+  ${STREAMLINK} "https://sp.gch.jp/#ch${channel_code}" \
     --greenchannel-email="${GREENCH_EMAIL}" \
     --greenchannel-password="${GREENCH_PASSWORD}" \
     best --stream-url
