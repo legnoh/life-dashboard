24aa4e3b0380a0e49d393ccb897e7e6943697704
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2025-03-03 07:49:46 +0900

    add unified reminders panel

--- roles/start_grafana_config/files/grafana/grafana_panel-reminders.tf
+++ roles/start_grafana_config/files/grafana/grafana_panel-reminders.tf
@@ -51,3 +51,43 @@ resource "grafana_library_panel" "reminders-asap" {
     })
   }))
 }
+
+resource "grafana_library_panel" "reminders-all" {
+  org_id     = grafana_organization.main.org_id
+  folder_uid = grafana_folder.todolist.uid
+  name       = "リマインダー - タスク残数"
+  model_json = jsonencode(merge(local.common_base, local.stats_base, {
+    title = "タスク残数",
+    targets = [
+      merge(local.target_base, {
+        expr   = "reminders_list_total{name=\"ASAP #私用\"}"
+        format = "time_series"
+        refId  = "ASAP #私用"
+      }),
+      merge(local.target_base, {
+        expr   = "reminders_all_total{status=\"outdated\"}"
+        format = "time_series"
+        refId  = "期限切れ"
+      }),
+    ],
+    transformations = [{
+      id = "renameByRegex"
+      options = {
+        regex = "Value #(.*)"
+        renamePattern = "$1"
+      }
+    }]
+    fieldConfig = merge(local.field_config_base, {
+      defaults = merge(local.field_config_default_base, {
+        thresholds = merge(local.thresholds_base, {
+          steps = [
+            zipmap(local.thresholds_keys, ["green", null]),
+            zipmap(local.thresholds_keys, ["yellow", 5]),
+            zipmap(local.thresholds_keys, ["red", 10]),
+          ]
+        })
+        unit = "個"
+      })
+    })
+  }))
+}
