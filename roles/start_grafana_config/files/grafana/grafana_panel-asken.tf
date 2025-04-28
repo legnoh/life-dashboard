e9b95d346d9d91be93e0aec5f90eb27c7fefcb7b
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2025-04-28 10:46:34 +0900

    fix threshold of asken

--- roles/start_grafana_config/files/grafana/grafana_panel-asken.tf
+++ roles/start_grafana_config/files/grafana/grafana_panel-asken.tf
@@ -11,13 +11,13 @@ resource "grafana_library_panel" "asken-score" {
       defaults = merge(local.field_config_default_base, {
         thresholds = merge(local.thresholds_base, {
           steps = [
-            zipmap(local.thresholds_keys, ["red", null]),
-            zipmap(local.thresholds_keys, ["orange", 30]),
-            zipmap(local.thresholds_keys, ["yellow", 40]),
-            zipmap(local.thresholds_keys, ["super-light-green", 50]),
-            zipmap(local.thresholds_keys, ["green", 60]),
-            zipmap(local.thresholds_keys, ["semi-dark-green", 70]),
-            zipmap(local.thresholds_keys, ["dark-green", 80]),
+            zipmap(local.thresholds_keys, ["dark-red", null]),
+            zipmap(local.thresholds_keys, ["red", 30]),
+            zipmap(local.thresholds_keys, ["orange", 40]),
+            zipmap(local.thresholds_keys, ["yellow", 50]),
+            zipmap(local.thresholds_keys, ["super-light-green", 60]),
+            zipmap(local.thresholds_keys, ["light-green", 70]),
+            zipmap(local.thresholds_keys, ["green", 80]),
           ]
         })
       })
